527e7d884892aa79b9d84f66d59ed5630401cd43
Kauan Farias <Kauan.farias.2505@gmail.com>
2023-04-17 18:16:49 -0300

    finalização dos campos

--- interações/form.js
+++ interações/form.js
@@ -4,27 +4,35 @@
     $('#date').mask('00/00/0000');
 
     //Meu
-    var password = window.document.querySelector('input#senha')
-    var passconf = window.document.querySelector('input#senhaconfirm')
-    var icon1 = window.document.querySelector('div#icon1')
-    var icon2 = window.document.querySelector('div#icon2')
+    var password = window.document.querySelector('input#senha');
+    var passconf = window.document.querySelector('input#senhaconfirm');
+    var icon1 = window.document.querySelector('div#icon1');
+    var icon2 = window.document.querySelector('div#icon2');
 
     function showHide1(){
       if(password.type === 'password'){
         password.setAttribute('type','text');
-        icon1.classList.add('hide')
+        passconf.setAttribute('type','text');
+        icon1.classList.add('hide');
+        icon2.classList.add('hide');
       } else {  
         password.setAttribute('type','password');
-        icon1.classList.remove('hide')
+        passconf.setAttribute('type','password');
+        icon1.classList.remove('hide');
+        icon2.classList.remove('hide');
       }
     }
 
     function showHide2(){
       if(passconf.type === 'password'){
         passconf.setAttribute('type','text');
-        icon2.classList.add('hide')
+        password.setAttribute('type','text');
+        icon2.classList.add('hide');
+        icon1.classList.add('hide');
       } else {  
         passconf.setAttribute('type','password');
-        icon2.classList.remove('hide')
+        password.setAttribute('type','text');
+        icon2.classList.remove('hide');
+        icon1.classList.remove('hide');
       }
     }
