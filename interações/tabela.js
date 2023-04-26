9a2ecaed213b60ad275f042b48e90b00ebdf8be6
Kauan Farias <Kauan.farias.2505@gmail.com>
2023-04-26 12:35:40 -0300

    novo menu

--- interações/tabela.js
+++ interações/tabela.js
@@ -1,8 +1,27 @@
 
-function menu(){
-    let menu = window.document.querySelector('va')
-    
-    menu.classList.add('menu-show')
-    console.log('1')
-    
+function areaDados1(){
+    let area1 = window.document.getElementById('cadastrar-dados1')
+    if(area1.style.display == 'none'){
+        area1.style.display = 'block'
+    } else {
+        area1.style.display = 'none'
+    }
+}
+
+function areaDados2(){
+    let area1 = window.document.getElementById('cadastrar-dados2')
+    if(area1.style.display == 'none'){
+        area1.style.display = 'block'
+    } else {
+        area1.style.display = 'none'
+    }
+}
+
+function areaDados3(){
+    let area1 = window.document.getElementById('cadastrar-dados3')
+    if(area1.style.display == 'none'){
+        area1.style.display = 'block'
+    } else {
+        area1.style.display = 'none'
+    }
 }
