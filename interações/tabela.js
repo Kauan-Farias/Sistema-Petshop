101b4f17fdf12c293e4b24256ce302f458a8a72d
Kauan Farias <Kauan.farias.2505@gmail.com>
2023-04-30 20:29:01 -0300

    tabela

---
+++ interações/tabela.js
@@ -0,0 +1,35 @@
+function menu(){
+    let menu = window.document.querySelector('nav.menu-hide')
+    if(menu.style.display == 'none'){
+        menu.style.display = 'block'
+    } else {
+        menu.style.display = 'none'
+    }
+}
+
+function areaDados1(){
+    let area1 = window.document.getElementById('cadastrar-dados1')
+    if(area1.style.display == 'none'){
+        area1.style.display = 'block'
+    } else {
+        area1.style.display = 'none'
+    }
+}
+
+function areaDados2(){
+    let area1 = window.document.getElementById('cadastrar-dados2')
+    if(area1.style.display == 'none'){
+        area1.style.display = 'block'
+    } else {
+        area1.style.display = 'none'
+    }
+}
+
+function areaDados3(){
+    let area1 = window.document.getElementById('cadastrar-dados3')
+    if(area1.style.display == 'none'){
+        area1.style.display = 'block'
+    } else {
+        area1.style.display = 'none'
+    }
+}
